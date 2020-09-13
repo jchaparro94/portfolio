8e4a39d9a1f5a62a0fbec3aa2be7cef3ee3192f7
Jose <chaparro.jjr@gmail.com>
2020-09-13 11:41:20 -0700

    added media queries for larger screens. Added attributes to the contact form for functionality

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,11 +2,16 @@ let navList = document.querySelector('.nav-list');
 let timesButton = document.querySelector('.fa-times');
 let burger = document.querySelector('.fa-bars');
 let hamburgerMenu = document.querySelector('.hamburger-menu');
-
-
+let navLink = document.querySelectorAll('.nav-link');
 
 // Hamburger Toggler
 hamburgerMenu.addEventListener('click', function () {
+  navList.classList.toggle('menu-open');
+  timesButton.classList.toggle('show');
+  burger.classList.toggle('hide');
+});
+
+navLink.addEventListener('click', function () {
   navList.classList.toggle('menu-open');
   timesButton.classList.toggle('show');
   burger.classList.toggle('hide');
